5bd2b0bb4ed9605874b189a00ee57246c7fde602
yslaoui <yslaouiyassine@gmail.com>
2024-04-17 16:30:33 +0100

    reading csv file and using Collections framework to put data into a List of String arrays

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -1,8 +1,27 @@
 package javabankapp;
 
+import utilities.Csv;
+
+import java.util.List;
+
 public class BankAccountApp {
     public static void main(String[] args) {
-        Checkings chk1 = new Checkings("Tom Wilson", "15874569823", 5000);
+        List<String[]> data =  Csv.read("C:\\Documents\\java\\projects\\bankaccountapp\\bankaccoutapp\\src\\javabankapp\\NewBankAccounts.csv");
+        for (String[] row: data ) {
+            String name = row[0];
+            String ssn = row[1];
+            String type = row[2];
+            String amount = row[3];
+            if (type.equals("Savings")) {
+                System.out.println("OPEN A SAVINGS ACCOUNT");
+            }
+            else {
+                System.out.println("OPEN A CHECKING ACCOUNT");
+            }
+            System.out.println(name + " " + ssn + " " + type + " " + amount + "$");
+        }
+
+        /*        Checkings chk1 = new Checkings("Tom Wilson", "15874569823", 5000);
         chk1.setDebitCard();
         chk1.showInfo();
 
@@ -13,9 +32,10 @@ public class BankAccountApp {
         sav1.deposit(5000);
         sav1.withdraw(100);
         sav1.transfer(1700, "Ghissa");
-        sav1.compound();
+        sav1.compound();*/
 
     }
 }
+
 
 
